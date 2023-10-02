3c92f5fb26d7eb3c71d9342a677a125569f301df
robfil50219 <robfil50219@stud.noroff.no>
2023-10-02 14:00:09 +0200

    strings,boolean and arrey poperties

---
+++ js/MyCar.js
@@ -0,0 +1,20 @@
+const myCar = {
+    year: 2018,
+    make: "Citroen",
+    engine: "PureTech 110 hp",
+    transmition: "Automatic",
+    isRunning: false, 
+    features: [
+        "Air Conditioning",
+        "Bluetooth Connectivety", 
+        "Apple Carplay",
+        "GPS Navigation",
+        "Leather Seats",
+        "Electric Memory Seats",
+        "Sunroof",
+    ],
+
+    //function to start the car 
+
+
+}
